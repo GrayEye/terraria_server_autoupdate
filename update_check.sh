97f162f208f9b6c1c7f9835a367c4f1921bc0614
Graydon <graydonneill@Gmail.com>
2022-11-25 17:22:19 -0500

    Update update_check.sh
    better way of getting server name

--- update_check.sh
+++ update_check.sh
@@ -2,5 +2,6 @@
 set -euxo pipefail
 
 RESULT="`wget -qO- https://terraria.org/api/get/dedicated-servers-names`"
-CURRENT="$(echo $RESULT | sed 's/^[^"]*"\([^"]*\)".*/\1/')"
+CURRENT="$(echo $RESULT | jq -r '.[0]')"
+#CURRENT="$(echo $RESULT | sed 's/^[^"]*"\([^"]*\)".*/\1/')"
 echo "$CURRENT" > current_server_version.txt
