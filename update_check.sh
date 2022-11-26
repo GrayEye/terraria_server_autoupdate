5e77d3f5c84754a69c83a1b04eaf92a9b8f2954a
gray <GrayEye@users.noreply.github.com>
2022-11-26 02:49:58 +0000

    continued work

--- update_check.sh
+++ update_check.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
-set -euxo pipefail
 
+if [ "./current_server_version.txt" ]
+  then
+    :
+  else
+    touch current_server_version.txt
+fi
+OLD=$(<current_server_version.txt)
 RESULT="`wget -qO- https://terraria.org/api/get/dedicated-servers-names`"
 CURRENT="$(echo $RESULT | jq -r '.[0]')"
-#CURRENT="$(echo $RESULT | sed 's/^[^"]*"\([^"]*\)".*/\1/')"
 echo "$CURRENT" > current_server_version.txt
+if [ $OLD == $CURRENT ]
+  then
+    exit 0
+  else
+    echo 'no'
+fi
