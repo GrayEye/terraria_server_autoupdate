4723cf7d1b3a3ffa7a493ef91a3a79330df38101
Graydon <graydonneill@Gmail.com>
2022-12-14 15:20:13 -0500

    Update update_check.sh

--- update_check.sh
+++ update_check.sh
@@ -11,17 +11,15 @@ OLD=$(<~/terraria_server_autoupdate/current_server_version.txt)
 RESULT="`wget -qO- https://terraria.org/api/get/dedicated-servers-names`"
 CURRENT="$(echo $RESULT | jq -r '.[0]')"
 echo "$CURRENT" > ~/terraria_server_autoupdate/current_server_version.txt
-if [ "$OLD" == "$CURRENT" ]
-  then
-    :
-  else
-    rm ~/terraria_server_autoupdate/Dockerfile
-    sed -e "s/placeholder_1/$CURRENT/g" Dockerfile.template > Dockerfile
-    VERSION=${CURRENT##*-}
-    VERSION=${VERSION%%.zip}
-    sed -i "s/placeholder_2/$VERSION/g" Dockerfile
-    docker stop terraria
-    docker image rm terraria_local
-    docker build -t terraria_local ~/terraria_server_autoupdate/.
-    docker run -d -it --rm -p 7777:7777 -v $HOME/terraria/world:/root/.local/share/Terraria/Worlds --name="terraria" -e WORLD_FILENAME=New_World.wld terraria_local
+if [ "$OLD" != "$CURRENT" ]: then
+  #Look into passing vaiables in the Docker Build
+  rm ~/terraria_server_autoupdate/Dockerfile
+  sed -e "s/placeholder_1/$CURRENT/g" Dockerfile.template > Dockerfile
+  VERSION=${CURRENT##*-}
+  VERSION=${VERSION%%.zip}
+  sed -i "s/placeholder_2/$VERSION/g" Dockerfile
+  docker stop terraria
+  docker image rm terraria_local
+  docker build -t terraria_local ~/terraria_server_autoupdate/.
+  docker run -d -it --rm -p 7777:7777 -v $HOME/terraria/world:/root/.local/share/Terraria/Worlds --name="terraria" -e WORLD_FILENAME=New_World.wld terraria_local
 fi
